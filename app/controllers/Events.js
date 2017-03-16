944b036790b36d82115a5de658689d02e7ef414c
Cassandra Claire <r@sqr.cat>
2017-03-17 00:42:38 +1100

    This Commit: Added get request to events controller

--- app/controllers/Events.js
+++ app/controllers/Events.js
@@ -2,3 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 const Event = require('./models/events');
+
+
+router.get('/:eventId', (req, res) => {
+    const eventId = req.params.eventId;
+    Event.findEventById(eventId, (err, _event) => {
+        if (!_event) return res.status(400).send("There's no user here.");
+        return res.send(_event);
+    })
+})
