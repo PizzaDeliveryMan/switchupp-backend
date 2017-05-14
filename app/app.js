f718cdaf284bc8589d1ea79e80231d21863d9706
Mike <limpeh-sh@scryptmail.com>
2017-05-15 03:29:35 +1000

    Image File Size upload thingy fixed, limited the full report post request to 50mb. I think backend is a masterpiece now. Praise Kek

--- app/app.js
+++ app/app.js
@@ -1,25 +1,25 @@
-const express       = require('express');
-const path          = require('path');
-const logger        = require('morgan');
-const cookieParser  = require('cookie-parser');
-const bodyParser    = require('body-parser');
-const chai          = require('chai');
-const connect       = require('connect');
-const favicon       = require('serve-favicon');
-const mongoose      = require('mongoose');
+const express       = require('express')
+const path          = require('path')
+const logger        = require('morgan')
+const cookieParser  = require('cookie-parser')
+const bodyParser    = require('body-parser')
+const chai          = require('chai')
+const connect       = require('connect')
+const favicon       = require('serve-favicon')
+const mongoose      = require('mongoose')
 
 
-const cors          = require('cors');
+const cors          = require('cors')
 
 
 
 // START ROUTES
-const user          = require('./controllers/User');
-const report        = require('./controllers/Report');
+const user          = require('./controllers/User')
+const report        = require('./controllers/Report')
 
 //END ROUTES
 
-const app = express();
+const app = express()
 //Set views path
 // app.set(__dirname, 'views');
 //Set public path
@@ -28,23 +28,25 @@ const app = express();
 //app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')))
 //Set pug as view engine
 // app.set('view engine', 'pug
-app.use(cors());
+app.use(cors())
 app.options('*', cors()) // include before other routes
-app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({extended: true, limit: '50mb'})) // limits document size to 50mb, prevent error 413
+app.use(bodyParser.json({extended: true, limit: '50mb'})) // limits document size to 50mb, prevent error 413
+// app.use(bodyParser.json()) // for parsing application/json (not used since limits file size)
 //Routing
-app.use('/user', user);
+app.use('/user', user)
 app.use('/report', report)
 
 mongoose.connect('mongodb://userboi:password101@ds137281.mlab.com:37281/songstress-db', {}, (err) => {
 // mongoose.connect('mongodb://localhost:27017/songstress-db', {}, (err) => {
-    if (err) throw err;
+    if (err) throw err
 })
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 db.once('open', function() {
   // we're connected!
-  console.log('mLab db connected!');
+  console.log('mLab db connected!')
   // console.log('local db connected!');
 });
 
